d33c102638b27d86c229c0fc0b19bee413119a01
Isabelly Damascena <isabellydamascena@Isabellys-MacBook-Pro.local>
2022-07-08 10:39:20 -0300

    Add sticky footer
    Co-authored: @camilamariaoliveira

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# cookies-react
+This repository aims to be a poc to understand how to save a cookie in the browser 
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './CookieFooter.css'
+
+
+function CookieFooter() {
+  return (
+    <footer className='cookie-footer'>
+     <p>Some text warning the user to accept the cookies.</p>
+     <button onClick={(e) => console.log("Hi")}>Accept Cookies</button>
+    </footer>
+  );
+}
+
+
+function App() {
+  return (
+    <>
+      <p>Hello World!</p>
+      <CookieFooter/>
+    </>
+  );
+
+}
+
+export default App;
