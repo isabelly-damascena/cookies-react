50255403f0f74b28fde02075dfd47cfc4163710e
Isabelly Damascena <isabellydamascena@Isabellys-MacBook-Pro.local>
2022-07-09 20:05:26 -0300

    Add cookie to the browser and add a condition to show or not show the footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
 import './CookieFooter.css'
 
 
-function CookieFooter() {
-  return (
-    <footer className='cookie-footer'>
-     <p>Some text warning the user to accept the cookies.</p>
-     <button onClick={(e) => console.log("Hi")} className='accept-button'>Accept Cookies</button>
-    </footer>
-  );
-}
-
-
 function App() {
+
+  const [ cookies, setCookie, removeCokie ] = useCookies(['acceptedCookies']);
+
+  const handleCookieButtonClick = () => {
+    setCookie('acceptedCookies', true, { path: '/'})
+  }
+
+
   return (
     <>
-      <p>Hello World!</p>
-      <CookieFooter/>
+      <h2>Hello World!</h2>
+      { cookies.acceptedCookies && <p>User accepted the cookie</p>}
+      {!cookies.acceptedCookies && 
+        <footer className='cookie-footer'>
+          <p>Some text warning the user to accept the cookies.</p>
+          <button onClick={handleCookieButtonClick} className='accept-button'>Accept Cookies</button>
+       </footer>
+      }
     </>
   );
 
